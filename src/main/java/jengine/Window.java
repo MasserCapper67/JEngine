9c7c28fcb227b7a7367d9d1c58e2fbd949002f2f
massercapper67 <alvarobardajirobles@gmail.com>
2025-04-26 08:16:31 +0200

    Created mouse listener

--- src/main/java/jengine/Window.java
+++ src/main/java/jengine/Window.java
@@ -4,6 +4,7 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
@@ -32,6 +33,12 @@ public class Window {
 
         init();
         loop();
+
+        //Free the memory
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     private void init() {
